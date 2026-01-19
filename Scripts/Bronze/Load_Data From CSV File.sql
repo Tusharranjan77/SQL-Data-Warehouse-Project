cd676fec5fce1a45e279de39656e9d034109082c
Tusharranjan77 <tusharranjan77@gmail.com>
2026-01-19 17:08:53 +0530

    Create Load_Data From CSV File.sql

---
+++ Scripts/Bronze/Load_Data From CSV File.sql
@@ -0,0 +1,19 @@
+/* Delete all the rows in Table Bronze.crm_cust_info through Truncate;
+ Then load the  data from CSV file in your System;
+With Bulk Insert Function where First Row of CSV File is Skipped because it is Column Header
+AND Seprator would be Comma (,) */
+
+TRUNCATE TABLE Bronze.crm_cust_info;
+BULK INSERT Bronze.crm_cust_info
+FROM '<DATASET_PATH>/source_crm/customer_info.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+/* Note:
+Replace <DATASET_PATH> with your local dataset directory before execution. */
+
+SELECT COUNT(*) AS [Total Rows]
+FROM Bronze.crm_cust_info;
+
